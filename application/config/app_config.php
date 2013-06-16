8b76ea7c17e10aacc605b5b85e8b62efbc35ce98
barovski <barovski_kn@abv.bg>
2013-06-16 22:27:35 +0300

    sync

--- application/config/app_config.php
+++ application/config/app_config.php
@@ -5,7 +5,6 @@ $config['Site_name']='Shopping list';
 $config['Site_description']='Site description';
 $config['Site_keywords']='Site keywords';
 $config['Site_title']='Shopping list demo';
-$config['upload_path'] = './uploads/';
 
 
 
