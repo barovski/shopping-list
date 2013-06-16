65cc1783c8e0e81625024e4654d4c3b3a16b8e0c
barovski <barovski_kn@abv.bg>
2013-06-16 22:25:39 +0300

    Demo meta tags

--- application/config/app_config.php
+++ application/config/app_config.php
@@ -1,10 +1,10 @@
 <?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 
 
-$config['Site_name']='Spinglass Management Group Secure Data Room';
+$config['Site_name']='Shopping list';
 $config['Site_description']='Site description';
 $config['Site_keywords']='Site keywords';
-$config['Site_title']='Secure Data Room';
+$config['Site_title']='Shopping list demo';
 $config['upload_path'] = './uploads/';
 
 
