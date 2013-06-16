51a1586cc64b679df1b07ddaf6a979f4c4dee715
barovski <barovski_kn@abv.bg>
2013-06-16 21:05:34 +0300

    Update database.php
    remove public setings

--- application/config/database.php
+++ application/config/database.php
@@ -49,8 +49,8 @@ $active_group = 'default';
 $active_record = TRUE;
 
 $db['default']['hostname'] = 'localhost';
-$db['default']['username'] = 'root';
-$db['default']['password'] = '919191kr';
+$db['default']['username'] = '';
+$db['default']['password'] = '';
 $db['default']['database'] = 'shopping-list';
 $db['default']['dbdriver'] = 'mysql';
 $db['default']['dbprefix'] = '';
